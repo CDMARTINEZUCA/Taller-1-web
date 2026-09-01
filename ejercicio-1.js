4f64ca36ff664655596dd3ed2d128c93b83b51a7
Will1806 <00041625@uca.edu.sv>
2026-09-01 14:53:45 -0600

    fix: arreglango nombres de variables

--- ejercicio-1.js
+++ ejercicio-1.js
@@ -3,8 +3,8 @@ const nombreProducto = prompt("Ingresa el nombre del producto:");
 const precioProducto = Number(prompt("Ingresa el precio del producto ($):"));
 const porcentajeDescuento = Number(prompt("Ingresa el porcentaje de descuento (%):"));
 
-const montoDescuento = (precioOriginal * porcentajeDescuento) / 100;
-const precioFinal = precioOriginal - montoDescuento;
+const montoDescuento = (precioProducto * porcentajeDescuento) / 100;
+const precioFinal = precioProducto - montoDescuento;
 
 console.log("--- Resumen de Compra ---");
 console.log("Producto: ", nombreProducto);
